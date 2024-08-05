26632fb1cb2489c92429893d909429fbb8480b14
Daniel R <daniel.razavi@utoronto.ca>
2024-08-05 15:16:04 -0400

    generating slides.

--- instructional_team/generate_slides.sh
+++ instructional_team/generate_slides.sh
@@ -2,7 +2,7 @@
 
 # CONFIGURATION
 folder_md="./markdown_slides"
-folder_output="../slides/" # This will be used for both PDF and HTML
+folder_output="../slides" # This will be used for both PDF and HTML
 
 # Clear the screen for the splash screen
 clear
